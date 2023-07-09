ef7c82d1459c07803bf0aaa74e774f01b2c507eb
Akram Ali <akram.exp02@gmail.com>
2023-07-09 23:09:12 +0530

    Add infinite images

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
 // Unsplash API
 const count = 10;
-const apiKey = 'RYnJm0F08Z1fY1LLRe4U5_7b6VvLepkplSCOB7hZaLY';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+const apiKey = "RYnJm0F08Z1fY1LLRe4U5_7b6VvLepkplSCOB7hZaLY";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    // Create <a> to link to full photo
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // Create <img> for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 // get photos from api
 async function getPhotos() {
-    try {
-        const response = await fetch(apiUrl);
-        const data = response.json();
-        console.log(data);
-    } catch (error) {
-        
-    }
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {}
 }
 
+window.addEventListener('scroll', () => {
+  if(window.innerHeight +window.scrollY >=  document.body.offsetWidth - 1000) {
+    getPhotos();
+  }
+});
 getPhotos();
